c8bab839113ce55404c0df5baec5374de773915c
rabeeh-mv <rabeehmv333@gmail.com>
2024-06-05 09:19:17 +0700

    First

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// =========== show menu ===============
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+// -------menu show --------
+// Validate if constant exists 
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+// ========== menu hidden ==========
+// validate if constant exists
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+// ======== Remove Mobile nav ========
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //When we click on eack nav__link, we remove the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const links = document.querySelectorAll('a');
+
+        // Loop through each link and change the target attribute to _self
+        links.forEach(link => {
+            link.setAttribute('target', '_self');
+        });
